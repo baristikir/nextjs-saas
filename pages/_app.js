e59c1b3345f2d8212e7d9612391317eaf28ada2c
Baris Tikir <brstkr@icloud.com>
2020-11-23 23:58:54 +0100

    Global Styles Added :rocket:

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,38 @@
+import Head from "next/head";
 import { AuthProvider } from '@/lib/auth';
-import { ThemeProvider } from '@emotion/react';
+import { ThemeProvider, Global, css } from '@emotion/react';
 import theme from '@/styles/theme';
 import { CSSReset } from '@chakra-ui/react';
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <AuthProvider>
-        <CSSReset />
+        <GlobalStyle />
         <Component {...pageProps} />
       </AuthProvider>
     </ThemeProvider>
